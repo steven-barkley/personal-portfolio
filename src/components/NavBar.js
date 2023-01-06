53f1a473799af5ce553fd6d88c2ef4993c7841bf
Steven Barkley <steven.barkley@outlook.com>
2023-01-05 19:19:13 -0500

    Second commit, began the Navbar and CSS styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,51 @@
+import { useState, useEffect } from 'react';
+
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const NavBar = () => {
+    const [ activeLink, setActiveLink ] = useState( 'home' );
+    const [ scolled, seScrolled ] = useState( false );
+
+    useEffect( ( => {
+        const onScroll = () => {
+            if ( window.scrollY > 50 ) {
+                seScrolled( true );
+            } else {
+                seScrolled( false )
+            }
+        }
+
+        window.addEventListener( "scroll", onScroll );
+
+        return () => window.removeEventListener( "scroll", onScroll );
+    } ), [] )
+
+    const onUpdateActiveLink = ( value ) => {
+        setActiveLink( value );
+    }
+
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className={ scrolled ? "scrolled" : "" }>
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={ '' } alt="Logo" />
+                    <img src={ logo } alt="Logo" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#home" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink( 'home' ) }>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={ activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink( 'skills' ) } >Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={ activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink( 'projects' ) } > Projects</Nav.Link>
                         {/*         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
@@ -32,11 +60,14 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className='social-icon'>
-                            <a href="#"></a>
+                            <a href="#"><img src={ navIcon1 } alt="" /></a>
+                            <a href="#"><img src={ navIcon2 } alt="" /></a>
+                            <a href="#"><img src={ navIcon3 } alt="" /></a>
                         </div>
+                        <button className='vvd' onClick={ () => console.log( 'connect' ) }><span>Let's Connect</span></button>
                     </span>
                 </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            </Container >
+        </Navbar >
     );
 }
